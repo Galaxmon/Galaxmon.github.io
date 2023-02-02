506732e31b0701259b854174bc7d0f881e7fe127
Galaxmon <cristsolba5@gmail.com>
2023-02-01 19:02:10 -0600

    Update

--- assets/data/roses-remix-senpai/SongName__Port.lua
+++
@@ -1,8 +0,0 @@
-function onCreate()
-makeLuaText('watermark', songName .. " | Blantados", 0, 25, 670);
-makeLuaText('watermark2',"Scripts | Uhard999", 0, 25, 690); -- You can edit the created by Prisma Text just dont change anything else
-    setTextSize('watermark', 20);
-    addLuaText('watermark');
-    setTextSize('watermark2', 20);
-    addLuaText('watermark2');
-end
